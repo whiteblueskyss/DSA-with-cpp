113dd95405c16f11e1e4500c3ba917bc0d3a1517
MD SELIM <124541718+whiteblueskyss@users.noreply.github.com>
2024-01-06 11:57:10 +0600

    cpp basic range

---
+++ cpp_basics.cpp
@@ -0,0 +1,20 @@
+//Author: whiteblueskyss
+//Created Date:   2024-01-02 13:06:53
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // precedence
+    // double>> float>>ll int >>int>>char
+    // -10^9 <int< 10^9
+    // -10^12 <long int< 10^12
+    // -10^18 <long long int< 10^18
+    cout<<fixed<<setprecision(2)<<1e5<<endl;
+    // in string use pushback s.push_back(a); instead of using s += 'a';
+
+    // array size, locally 10^5 globally 10^7;
+
+    //(a-b)%M = ((a%M - b%M)+M)%M
+    
+    return 0;
+}
