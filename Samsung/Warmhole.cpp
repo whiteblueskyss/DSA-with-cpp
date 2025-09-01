e662d1cc45427df5b23f738bed6c01ef88f50db8
Md Selim <whiteblueskyss@gmail.com>
2025-09-01 14:06:23 +0600

    Warmhole updated

--- Samsung/Warmhole.cpp
+++ Samsung/Warmhole.cpp
@@ -1,68 +1,55 @@
-//Author: whiteblueskyss
-//Created Date:   2025-07-03 15:37:19
-//Last Modified: 2025-07-03 16:51:12
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <climits>
 using namespace std;
-#define FAST ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define ll long long int
-#define F first
-#define S second
-#define PB push_back
-#define PPB pop_back
-#define MP make_pair
-#define endl "\n"
-const int N = 2e5+10;
-const ll MOD = 1e9+7;
 
-ll sx,sy,ex,ey,n, ans=INT_MAX;
-vector<bool>vis(15, false); 
-vector<vector<ll>>v(15, vector<ll>(5));
+int n, sx, sy, dx, dy;
+vector<vector<int>> v; 
+int ans;
+bool vis[100];
 
-ll dis(int sx, int sy, int ex, int ey){
-    return abs(sx-ex) + abs(sy-ey);
-}
+void calc(int x, int y, int c) {
+    ans = min(ans, c + abs(x - dx) + abs(y - dy));
 
-void backtrack(ll sx, ll sy, ll ex, ll ey, ll val){
-    
-    ans = min(ans, dis(sx, sy, ex, ey)+val);
-
-    for(int i=0; i<n; i++){
-        if(!vis[i]){
+    for (int i = 0; i < n; i++) {
+        if (!vis[i]) {
             vis[i] = true;
-            ll temp = dis(sx,sy,v[i][0],v[i][1]) + v[i][4] + val;
-            backtrack(v[i][2], v[i][3], ex, ey, temp);
 
-            temp = dis(sx, sy, v[i][2], v[i][3]) + v[i][4] + val;
-            backtrack(v[i][0], v[i][1], ex, ey, temp);
+            // enter at (x1,y1), exit at (x2,y2)
+            int temp1 = c + abs(x - v[i][0]) + abs(y - v[i][1]) + v[i][4];
+            calc(v[i][2], v[i][3], temp1);
+
+            // enter at (x2,y2), exit at (x1,y1)
+            int temp2 = c + abs(x - v[i][2]) + abs(y - v[i][3]) + v[i][4];
+            calc(v[i][0], v[i][1], temp2);
 
             vis[i] = false;
         }
     }
 }
 
-void solve(){
-    cin>>n;
-    cin>>sx>>sy>>ex>>ey;
+void solve() {
+    cin >> n;
+    cin >> sx >> sy >> dx >> dy;
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<5; j++){
-            cin>>v[i][j];
-        }
+    v.assign(n, vector<int>(5));
+    ans = INT_MAX;
+    fill(begin(vis), end(vis), false);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i][0] >> v[i][1] >> v[i][2] >> v[i][3] >> v[i][4];
     }
-    
-    backtrack(sx, sy, ex, ey, 0); 
 
-    cout<<ans<<endl;
+    
+    calc(sx, sy, 0);
+
+    cout << ans << endl;
 }
 
-int main(){
-    FAST
-    int t=1;
+int main() {
+    int t;
     cin >> t;
-    for(int i=1; i<=t; i++){
-        // cout<<"Test: "<<i<<endl;
+    while (t--) {
         solve();
     }
-    return 0;
 }
