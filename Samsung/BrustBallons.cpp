4e891ead6e606d775306fbb76e24e83ddf0a4054
Md Selim <whiteblueskyss@gmail.com>
2025-09-01 14:23:56 +0600

    Updated Brustballong.cpp

--- Samsung/BrustBallons.cpp
+++ Samsung/BrustBallons.cpp
@@ -1,49 +1,115 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long int 
+#include <iostream>
+#include<vector>
+#include <climits>
 
-int f (vector<ll>&v, int i, int j, vector<vector<ll>>&dp){
-    if(i>j) return 0;
-    
+using namespace std;
+
+int n;
+
+int get(int i, int j, vector<vector<int>>&dp, vector<int>&v){
+    if(i>j){
+        return 0;
+    }
+
     if(dp[i][j]!=-1){
         return dp[i][j];
     }
 
-    ll mx = INT_MIN;
+    int ans = INT_MIN;
 
-    for(ll idx = i; idx<=j; idx++){
-        ll temp = v[i-1]*v[j+1];
-        ll n = v.size()-2;
-        if(i==1 && j==n){
+    for(int idx = i ; idx<=j; idx++){
+        int temp = v[i-1]*v[j+1];
+        // n = v.size()-2;
+        if(i == 1 && j ==n){
             temp = v[idx];
         }
 
-        ll score = temp + f(v, i, idx-1, dp) + f(v, idx+1, j, dp);
+        int score = temp + get(i, idx-1, dp, v) + get(idx+1, j, dp, v);
 
-        mx = max(score, mx);
-        // cout<<i<< " "<<j << " "<<mx<<score<<endl; 
+        ans = max(ans, score);
     }
 
-    return dp[i][j] = mx;
+    return dp[i][j] = ans;
+
 }
 
-
 int main(){
-    ll n;
     cin>>n;
+    vector<int>v(n);
 
-    vector<ll>v(n); 
-
-    for(ll i=0; i<n; i++){
+    for(int i=0; i<n; i++){
         cin>>v[i];
     }
 
     v.push_back(1);
-    v.insert(v.begin(), 1);
+    v.insert(v.begin(),1);
 
-    vector<vector<ll>>dp (n+2, vector<ll>(n+2, -1));
+    vector<vector<int>>dp(n+2, vector<int>(n+2, -1));
 
-    cout<<f(v,1,n, dp)<<endl;
+    int ans = get(1, n, dp, v);
 
-
+    cout<<ans<<endl;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define ll long long int 
+
+// int f (vector<ll>&v, int i, int j, vector<vector<ll>>&dp){
+//     if(i>j) return 0;
+    
+//     if(dp[i][j]!=-1){
+//         return dp[i][j];
+//     }
+
+//     ll mx = INT_MIN;
+
+//     for(ll idx = i; idx<=j; idx++){
+//         ll temp = v[i-1]*v[j+1];
+//         ll n = v.size()-2;
+//         if(i==1 && j==n){
+//             temp = v[idx];
+//         }
+
+//         ll score = temp + f(v, i, idx-1, dp) + f(v, idx+1, j, dp);
+
+//         mx = max(score, mx);
+//         // cout<<i<< " "<<j << " "<<mx<<score<<endl; 
+//     }
+
+//     return dp[i][j] = mx;
+// }
+
+
+// int main(){
+//     ll n;
+//     cin>>n;
+
+//     vector<ll>v(n); 
+
+//     for(ll i=0; i<n; i++){
+//         cin>>v[i];
+//     }
+
+//     v.push_back(1);
+//     v.insert(v.begin(), 1);
+
+//     vector<vector<ll>>dp (n+2, vector<ll>(n+2, -1));
+
+//     cout<<f(v,1,n, dp)<<endl;
+
+
+// }
